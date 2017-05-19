035486f33ace9bea99b43d13294e9ee2c60d74e6
Justin Kambic <justin@Justins-MacBook-Pro.local>
2017-05-19 10:47:20 -0400

    Added more unit tests for moves.

--- tests/engine/game.spec.cpp
+++ tests/engine/game.spec.cpp
@@ -13,20 +13,6 @@ TEST_CASE("Game Initialization", "[Game]") {
     REQUIRE(expectedBoard.getCurMove() == gameBoard.getCurMove());
 }
 
-TEST_CASE("Gets expected moves for white man", "[Game]") {
-    Game::Game g;
-    g.updateBoardState((unsigned long)1 << 17, (unsigned long)1 << 6, 'w');
-
-    Move::Move upLeft(6, 10, 'w', false);
-    Move::Move upRight(6, 11, 'w', false);
-
-    std::vector<std::shared_ptr<Move> > gMoves = g.getCurrentMoves();
-
-    REQUIRE(gMoves.size() == 2);
-    REQUIRE(upLeft == *(gMoves[0]));
-    REQUIRE(upRight == *(gMoves[1]));
-}
-
 TEST_CASE("Gets expected moves for black man", "[Game]") {
     Game::Game g;
     g.updateBoardState((unsigned long)1 << 17, (unsigned long)1 << 6, 'b');
@@ -41,22 +27,49 @@ TEST_CASE("Gets expected moves for black man", "[Game]") {
     REQUIRE(downRight == *(gMoves[1]));
 }
 
-TEST_CASE("Gets expected moves for white king", "[Game]") {
+TEST_CASE("Generates expected moves for initial turn", "[Game]") {
     Game::Game g;
-    g.updateBoardState(1 << 25, (unsigned long)1 << 45, 'w');
+    std::vector<Move> moves;
+    moves.push_back(Move(20, 16, 'b', false));
+    moves.push_back(Move(20, 17, 'b', false));
+    moves.push_back(Move(21, 17, 'b', false));
+    moves.push_back(Move(21, 18, 'b', false));
+    moves.push_back(Move(22, 18, 'b', false));
+    moves.push_back(Move(22, 19, 'b', false));
+    moves.push_back(Move(23, 19, 'b', false));
+    std::vector<std::shared_ptr<Move> > generatedMoves = g.getCurrentMoves();
 
-    Move::Move upLeft(13, 17, 'w', false);
-    Move::Move upRight(13, 18, 'w', false);
-    Move::Move downLeft(13, 9, 'w', false);
-    Move::Move downRight(13, 10, 'w', false);
+    std::vector<Move>::iterator it;
+    
+    for (it = moves.begin(); it < moves.end(); it++)
+    {
+        bool found = false;
+        for (size_t i = 0; i < generatedMoves.size(); i++)
+        {
+            if (it->getFrom() == generatedMoves[i]->getFrom()
+                && it->getTo() == generatedMoves[i]->getTo()
+                && it->getSide() == generatedMoves[i]->getSide())
+                {
+                    found = true;
+                    break;
+                }
+        }
+        REQUIRE(found);
+    }
+}
+
+TEST_CASE("Gets expected moves for white man", "[Game]") {
+    Game::Game g;
+    g.updateBoardState((unsigned long)1 << 17, (unsigned long)1 << 6, 'w');
+
+    Move::Move upLeft(6, 10, 'w', false);
+    Move::Move upRight(6, 11, 'w', false);
 
     std::vector<std::shared_ptr<Move> > gMoves = g.getCurrentMoves();
 
-    REQUIRE(gMoves.size() == 4);
+    REQUIRE(gMoves.size() == 2);
     REQUIRE(upLeft == *(gMoves[0]));
     REQUIRE(upRight == *(gMoves[1]));
-    REQUIRE(downRight == *(gMoves[2]));
-    REQUIRE(downLeft == *(gMoves[3]));
 }
 
 TEST_CASE("Gets expected moves for black king", "[Game]") {
@@ -75,6 +88,24 @@ TEST_CASE("Gets expected moves for black king", "[Game]") {
     REQUIRE(upRight == *(gMoves[1]));
     REQUIRE(downLeft == *(gMoves[2]));
     REQUIRE(downRight == *(gMoves[3]));
+}
+
+TEST_CASE("Gets expected moves for white king", "[Game]") {
+    Game::Game g;
+    g.updateBoardState(1 << 25, (unsigned long)1 << 45, 'w');
+
+    Move::Move upLeft(13, 17, 'w', false);
+    Move::Move upRight(13, 18, 'w', false);
+    Move::Move downLeft(13, 9, 'w', false);
+    Move::Move downRight(13, 10, 'w', false);
+
+    std::vector<std::shared_ptr<Move> > gMoves = g.getCurrentMoves();
+
+    REQUIRE(gMoves.size() == 4);
+    REQUIRE(upLeft == *(gMoves[0]));
+    REQUIRE(upRight == *(gMoves[1]));
+    REQUIRE(downRight == *(gMoves[2]));
+    REQUIRE(downLeft == *(gMoves[3]));
 }
 
 TEST_CASE("Generates expected jumps", "[Game]") {
@@ -109,7 +140,35 @@ TEST_CASE("Chained jumps", "[Game]") {
     REQUIRE(*child == expectedChild);
 }
 
-TEST_CASE("Multiple chained jumps", "[Game]") {
+TEST_CASE("Can jump white king with black man", "[Game]") {
+    Game::Game g;
+    // black 20
+    // white 17, king
+    g.updateBoardState(1048576, (unsigned long)1 << 49);
+
+    Move::Move expected(20, 17, 'b', true);
+
+    std::vector<std::shared_ptr<Move> > generatedMoves = g.getCurrentMoves();
+
+    REQUIRE(generatedMoves.size() == 1);
+    REQUIRE(*(generatedMoves.front()) == expected);
+}
+
+TEST_CASE("Can jump black king with white man", "[Game]") {
+    Game::Game g;
+    // black 17
+    // white 13
+    g.updateBoardState((unsigned long)1 << 49, 8192, 'w');
+    
+    Move::Move expected(13, 17, 'w', true);
+
+    std::vector<std::shared_ptr<Move> > generatedMoves = g.getCurrentMoves();
+
+    REQUIRE(generatedMoves.size() == 1);
+    REQUIRE(*(generatedMoves.front()) == expected);
+}
+
+TEST_CASE("Multiple chained jumps for black king", "[Game]") {
     Game::Game g;
     g.updateBoardState((unsigned long)1 << 56, 1060880);
 
@@ -137,33 +196,36 @@ TEST_CASE("Multiple chained jumps", "[Game]") {
     REQUIRE(grandChild == *gGrandChild);
 }
 
-TEST_CASE("Generates expected moves", "[Game]") {
+TEST_CASE("Multiple chained jumps for white king", "[Game]") {
     Game::Game g;
-    std::vector<Move> moves;
-    moves.push_back(Move(20, 16, 'b', false));
-    moves.push_back(Move(20, 17, 'b', false));
-    moves.push_back(Move(21, 17, 'b', false));
-    moves.push_back(Move(21, 18, 'b', false));
-    moves.push_back(Move(22, 18, 'b', false));
-    moves.push_back(Move(22, 19, 'b', false));
-    moves.push_back(Move(23, 19, 'b', false));
+    // black needs 5, 13, 14, 20, 21
+    // white needs 1
+    g.updateBoardState((unsigned long)17592189214752, (unsigned long)1 << 33, 'w');
+
+    Move::Move parent(1, 5, 'w', true);
+    Move::Move child1(10, 13, 'w', true);
+    Move::Move child2(10, 14, 'w', true);
+    Move::Move gChild1(17, 20, 'w', true);
+    Move::Move gChild2(17, 21, 'w', true);
+    Move::Move gChild3(17, 12, 'w', true);
+
     std::vector<std::shared_ptr<Move> > generatedMoves = g.getCurrentMoves();
 
-    std::vector<Move>::iterator it;
-    
-    for (it = moves.begin(); it < moves.end(); it++)
-    {
-        bool found = false;
-        for (size_t i = 0; i < generatedMoves.size(); i++)
-        {
-            if (it->getFrom() == generatedMoves[i]->getFrom()
-                && it->getTo() == generatedMoves[i]->getTo()
-                && it->getSide() == generatedMoves[i]->getSide())
-                {
-                    found = true;
-                    break;
-                }
-        }
-        REQUIRE(found);
-    }
+    REQUIRE(generatedMoves.size() == 1);
+    REQUIRE(*(generatedMoves[0]) == parent);
+
+    std::shared_ptr<Move> childA = generatedMoves[0]->getChildren()[0];
+    std::shared_ptr<Move> childB = generatedMoves[0]->getChildren()[1];
+    REQUIRE(*(childA) == child1);
+    REQUIRE(*(childB) == child2);
+    REQUIRE(childA->getChildren().size() == 3);
+    REQUIRE(childB->getChildren().size() == 0);
+
+    std::shared_ptr<Move> gcA = childA->getChildren()[0];
+    std::shared_ptr<Move> gcB = childA->getChildren()[1];
+    std::shared_ptr<Move> gcC = childA->getChildren()[2];
+
+    REQUIRE(*(gcA) == gChild1);
+    REQUIRE(*(gcB) == gChild2);
+    REQUIRE(*(gcC) == gChild3);
 }
